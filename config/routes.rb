9ca8ccce831800cb4f3d46351b495b61c814c6b5
jiao.cui <jiao.cui@rea-group.com>
2015-12-24 18:00:57 +0800

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'users#index'
+
+  post 'users/login' => 'users#login', as: :user_login
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
