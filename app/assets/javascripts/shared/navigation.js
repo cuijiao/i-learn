110642a1e5e72ae225814b47129f05c17bf11f05
jiao.cui <jiao.cui@rea-group.com>
2016-01-13 18:17:51 +0800

    add navigation

---
+++ app/assets/javascripts/shared/navigation.js
@@ -0,0 +1,18 @@
+//@cjsx React.DOM
+
+var Navigation = React.createClass({
+    render: function(){
+        return(
+            <div>
+                <ul>
+                    <li>learn</li>
+                    <li>技能</li>
+                    <li>我的</li>
+                    <li>创建</li>
+                    <li>name</li>
+                    <li>search</li>
+                </ul>
+            </div>
+        );
+    }
+});
