ab2cd5e1cb962936db74301bc8b314244420bc95
jiao.cui <jiao.cui@rea-group.com>
2016-01-13 20:44:58 +0800

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,5 @@ Course.create({name:"CSS语言", tag:"learn",owner: "tong", published_at: Time.n
 Course.create({name:"Ruby", tag:"learn",owner: "tong", published_at: Time.new(2016,1,1), status: "open"})
 Course.create({name:"Mobile开发", tag:"learn",owner: "tong", published_at: Time.new(2016,1,1), status: "open"})
 Course.create({name:"Web开发", tag:"learn",owner: "tong", published_at: Time.new(2016,1,1), status: "open"})
-Course.create({name:"CSS语言", tag:"entertainment",owner: "jiao", published_at: Time.new(2016,1,1), status: "open"})
-Course.create({name:"Ruby", tag:"entertainment",owner: "jiao", published_at: Time.new(2016,1,1), status: "open"})
+Course.create({name:"Yoga", tag:"entertainment",owner: "jiao", published_at: Time.new(2016,1,1), status: "open"})
+Course.create({name:"太极拳", tag:"entertainment",owner: "jiao", published_at: Time.new(2016,1,1), status: "open"})
