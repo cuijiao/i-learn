bed3a3b06427aeabf675d4c29c30038ef0ee2292
jiao.cui <jiao.cui@rea-group.com>
2016-01-13 21:52:44 +0800

    add learned coursed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+ming = User.create({username:"Xiao Ming", password:"test"})
+
 Course.create({name:"CSS语言", tag:"learn",owner: "tong", published_at: Time.new(2016,1,1), status: "open"})
-Course.create({name:"Ruby", tag:"learn",owner: "tong", published_at: Time.new(2016,1,1), status: "open"})
+ruby = Course.create({name:"Ruby", tag:"learn",owner: "tong", published_at: Time.new(2016,1,1), status: "open"})
 Course.create({name:"Mobile开发", tag:"learn",owner: "tong", published_at: Time.new(2016,1,1), status: "open"})
 Course.create({name:"Web开发", tag:"learn",owner: "tong", published_at: Time.new(2016,1,1), status: "open"})
-Course.create({name:"Yoga", tag:"entertainment",owner: "jiao", published_at: Time.new(2016,1,1), status: "open"})
+yoga = Course.create({name:"Yoga", tag:"entertainment",owner: "jiao", published_at: Time.new(2016,1,1), status: "open"})
 Course.create({name:"太极拳", tag:"entertainment",owner: "jiao", published_at: Time.new(2016,1,1), status: "open"})
+
+RegistedCourse.create({user_id:ming.id,course_id:ruby.id})
+RegistedCourse.create({user_id:ming.id,course_id:yoga.id})
