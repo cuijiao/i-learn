a70a6a8fe6046e8b7b82293349d61aa044080339
Tong Shen <tong.shen@rea-group.com>
2016-01-14 13:08:03 +0800

    polish courses style

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20160113132009) do
     t.datetime "updated_at"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "registed_courses", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "course_id"
